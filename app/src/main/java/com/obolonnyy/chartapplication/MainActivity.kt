2936f8bc05503a5cc8f37fd7795a189a91924f36
Vladimir Obolonnyy <v.obolonnyy@alpacapital.ru>
2021-11-07 17:48:06 +0300

    compose works!

--- app/src/main/java/com/obolonnyy/chartapplication/MainActivity.kt
+++ app/src/main/java/com/obolonnyy/chartapplication/MainActivity.kt
@@ -2,10 +2,42 @@ package com.obolonnyy.chartapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.google.android.material.composethemeadapter.MdcTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val greeting = findViewById<ComposeView>(R.id.greeting)
+        greeting.setContent {
+            MdcTheme { // or AppCompatTheme
+                Greeting()
+            }
+        }
     }
+}
+
+@Composable
+private fun Greeting() {
+    Text(
+        text = stringResource(R.string.greeting),
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp)
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
